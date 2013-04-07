72b6a4a261a574dc1465849f19c1335d396b8d4f
Andrew414 <andrew414414@gmail.com>
2013-04-07 06:51:55 -0700

    Update module.c

--- module.c
+++ module.c
@@ -1,7 +1,7 @@
 #include "calc.h"
 
-static char names[][16] = {PROCFS_FIRST, PROCFS_SECOND, PROCFS_OPERAND, PROCFS_RESULT};
-static int indices[4];
+static char names[][PROCFS_MAX_SIZE] = {PROCFS_FIRST, PROCFS_SECOND, PROCFS_OPERAND, PROCFS_RESULT};
+static int indices[FILE_COUNT];
 static char** procfs_buffer;
 static struct proc_dir_entry** child_procs;
 
@@ -11,8 +11,8 @@ int __init module_load(void)
 
 	printk(KERN_INFO "Calculator module is loaded.\n");
 
-	procfs_buffer = (char**) kmalloc(sizeof(char*) * 4, GFP_KERNEL);
-	for (i = 0; i < 4; i++) {
+	procfs_buffer = (char**) kmalloc(sizeof(char*) * FILE_COUNT, GFP_KERNEL);
+	for (i = 0; i < FILE_COUNT; i++) {
 		procfs_buffer[i] = (char*) kmalloc(sizeof(char) * PROCFS_MAX_SIZE, GFP_KERNEL);
 		procfs_buffer[i][0] = '\0';
 	}
@@ -20,7 +20,7 @@ int __init module_load(void)
 	child_procs = (struct proc_dir_entry**) kmalloc(
 		sizeof(struct proc_dir_entry *) * 4, GFP_KERNEL);
 
-	for (i = 0; i < 4; i++) {
+	for (i = 0; i < FILE_COUNT; i++) {
 		child_procs[i] = create_proc_entry(names[i], 0644, NULL);
 		if (!child_procs[i]) {
 			remove_proc_entry(names[i], NULL);
@@ -41,7 +41,7 @@ void __exit module_unload(void)
 {	
 	int i;
 
-	for (i = 0; i < 4; i++) {
+	for (i = 0; i < FILE_COUNT; i++) {
 		remove_proc_entry(names[i], NULL);
 		kfree(procfs_buffer[i]);
 	}
@@ -49,8 +49,8 @@ void __exit module_unload(void)
 	kfree(child_procs);
 	kfree(procfs_buffer);
 
-	printk(KERN_INFO "Calculator processes were removed.\n");
-	printk(KERN_INFO "Calculator module is unloaded.\n");
+	printk(KERN_INFO "[CALC PROC] proc entries were removed.\n");
+	printk(KERN_INFO "[CALC PROC] module is unloaded.\nBye-bye!");
 }
 
 module_init(module_load);
